a9500bc72a18ad015d23b4717c3a8cd81ac0cf75
Aditya Bhatt <adityabhatt789@gmail.com>
2022-12-03 09:22:30 +0530

    added google-map API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+
+const center = {
+  lat: 26.913165,
+  lng: 75.803035
+};
+
+function MyComponent() {
+
+  const [data,setData] = useState([])
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyDvr_LmzYgdLaL7BgzxnZmln9TowpgW6z8'
+  })
+
+  function getCoordinates(address){
+    fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+address+'&key='+'AIzaSyDvr_LmzYgdLaL7BgzxnZmln9TowpgW6z8')
+      .then(response => response.json())
+      .then(data => {
+        const latitude = data.results[0].geometry.location.lat;
+        const longitude = data.results[0].geometry.location.lng;
+        console.log({latitude, longitude})
+      })
+  }
+
+  const [map, setMap] = React.useState(null)
+  const API_KEY = 'AIzaSyDvr_LmzYgdLaL7BgzxnZmln9TowpgW6z8'
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        onClick={getCoordinates}
+        // onClick={(e) => {
+    // console.log("latitude = ", e.lat)
+    // console.log(e.latLng)
+    // console.log("longtitude = ", e.lng);}}
+      >
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
 
-export default App;
+
+
+export default React.memo(MyComponent)
